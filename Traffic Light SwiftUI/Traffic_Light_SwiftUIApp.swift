b4e2959e2603a032289ab5b8178a3b2ec0b7c733
Demiar <alexey174@gmail.com>
2021-07-27 22:03:03 +0300

    Initial Commit

---
+++ Traffic Light SwiftUI/Traffic_Light_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_Light_SwiftUIApp.swift
+//  Traffic Light SwiftUI
+//
+//  Created by Алексей on 27.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_Light_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
